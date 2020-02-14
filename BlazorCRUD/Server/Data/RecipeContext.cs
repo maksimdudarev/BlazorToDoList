595e73d1a963199a364606f58e846c775be42d30
MD <maksim.dudarev@gmail.com>
2020-02-14 14:48:07 +0300

    add model context controller

---
+++ BlazorCRUD/Server/Data/RecipeContext.cs
@@ -0,0 +1,23 @@
+﻿using BlazorCRUDApp.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUDApp.Server.Data
+{
+    public class RecipeContext:DbContext
+    {
+        public virtual DbSet<Recipe> tblRecipe { get; set; }
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                //var connectionString = "Server=127.0.0.1;Port=5432;Database=recipe_db;User Id=postgres;Password = sa@123; CommandTimeout = 20";
+                var connectionString = "server=localhost;port=5432;database=blazorcrud;userid=postgres;password=1;";
+                optionsBuilder.UseNpgsql(connectionString);
+            }
+        }
+    }
+}
