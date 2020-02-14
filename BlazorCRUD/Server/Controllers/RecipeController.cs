3e19e962d4b47b307b9c3237c773ad21ecf459dc
MD <maksim.dudarev@gmail.com>
2020-02-14 16:14:45 +0300

    add pages

--- BlazorCRUD/Server/Controllers/RecipeController.cs
+++ BlazorCRUD/Server/Controllers/RecipeController.cs
@@ -1,46 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using BlazorCRUDApp.Server.Data;
+using BlazorCRUDApp.Shared.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace BlazorCRUD.Server.Controllers
+namespace BlazorCRUDApp.Server.Controllers
 {
     [Route("api/[controller]")]
     public class RecipeController : Controller
     {
+        private RecipeDbOperations dbOpereations = new RecipeDbOperations();
         // GET: api/<controller>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        [HttpGet("recipes")]
+        public IEnumerable<Recipe> GetRecipes()
         {
-            return new string[] { "value1", "value2" };
+            return dbOpereations.GetAllRecipes();
         }
 
         // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("detail/{id}")]
+        public Recipe GetRecipeDetail(int id)
         {
-            return "value";
+            return dbOpereations.GetRecipeDetail(id);
         }
 
         // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
+        [HttpPost("create")]
+        public void Post([FromBody]Recipe recipe)
         {
+            if (ModelState.IsValid)
+                dbOpereations.AddRecipe(recipe);
         }
 
         // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        [HttpPut("edit")]
+        public void Put([FromBody]Recipe recipe)
         {
+            if (ModelState.IsValid)
+                dbOpereations.UpdateRecipe(recipe);
         }
 
         // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
+        [HttpDelete("delete/{id}")]
         public void Delete(int id)
         {
+            dbOpereations.DeleteRecipe(id);
         }
     }
 }
